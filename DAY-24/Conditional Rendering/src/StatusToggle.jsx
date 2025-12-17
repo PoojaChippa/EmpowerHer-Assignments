148c17e0deaa73d809b0061f13857d9b7e98f854
PoojaChippa <poojachippa04@gmail.com>
2025-12-17 23:59:26 +0530

    added Conditional Rendering, Styling and Iteration

---
+++ DAY-24/Conditional Rendering/src/StatusToggle.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import ComponentA from "./ComponentA";
+import ComponentB from "./ComponentB";
+
+function StatusToggle() {
+  const [status, setStatus] = useState(false);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <button onClick={() => setStatus(!status)}>Toggle Status</button>
+
+      <br />
+      <br />
+
+      {status ? <ComponentA /> : <ComponentB />}
+    </div>
+  );
+}
+
+export default StatusToggle;
