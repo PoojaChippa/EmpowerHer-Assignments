fb9d4062df7cec0a4ed521f7435ea7ea928dabd7
PoojaChippa <poojachippa04@gmail.com>
2026-01-05 23:05:50 +0530

    added  tailwindcss exploration

---
+++ DAY-33/Tailwindcss Exploration/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <section className="p-8 text-center">
+      <h2 className="text-xl md:text-3xl lg:text-5xl font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+        Restaurant of Fresh Food
+      </h2>
+
+      <img
+        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fast-food-restaurant-logo%2C-restaurant-logo-design-template-33255790cb8e1186b28609dd9afd4ee6_screen.jpg?ts=1753722621"
+        className="mx-auto mt-6 w-40 rounded-lg shadow-lg transform transition hover:scale-105"
+      />
+    </section>
+  );
+}
+
+export default Hero;
